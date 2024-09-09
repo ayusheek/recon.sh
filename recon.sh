249d239fb19dfa0df65a5562aa314b497cf0782a
dat-ayush <ayushkr91997@gmail.com>
2024-09-09 16:11:19 +0530

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -5,20 +5,20 @@ create_cache_dir() {
 }
 
 subfinder_scan() {
-    domain=$1
-    output_file=$2
+    local domain=$1
+    local output_file=$2
     subfinder -d "$domain" -o "$output_file"
 }
 
 httpx_probe() {
-    hosts_file=$1
-    output_file=$2
+    local hosts_file=$1
+    local output_file=$2
     httpx -l "$hosts_file" -o "$output_file"
 }
 
 katana_crawl() {
-    live_hosts_file=$1
-    output_file=$2
+    local live_hosts_file=$1
+    local output_file=$2
     scan_time=$(date +"%Y-%m-%d")
 
     katana -list "$live_hosts_file" -ps -o ".cache/$scan_time/passive_crawled.txt"
@@ -31,11 +31,25 @@ nuclei_dast_scan() {
 }
 
 recon() {
-    domain=$1
-    output_dir=$2
+    local domain=$1
+    local output_dir=$2
     mkdir -p $output_dir
     subfinder_scan $domain $output_dir/"subs.txt"
     httpx_probe "subs.txt" $output_dir/"httpx.txt"
     katana_crawl $output_dir/"httpx.txt" $output_dir/"katana.txt"
     nuclei_dast_scan $output_dir/"katana.txt" $output_dir/"nuclei_dastScan_out.txt"
 }
+
+set -e
+
+if [[ $# -ne 2 ]]; then
+    echo -e "\nUsage: $0 <domain> <ouptut_dir>\n"
+    echo "Example:"
+    echo -e "  ./recon.sh example.com example\n"
+    exit 0
+fi
+
+# Usage: recon.sh <domain> <output_dir>
+
+set -e  # exit on any error
+recon "$1" "$2"
