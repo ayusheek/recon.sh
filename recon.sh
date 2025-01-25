0f294fea936125e86d1585edc870e9264427c2a3
ayushkr12 <ayushkr91997@gmail.com>
2025-01-25 18:48:09 +0530

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -12,6 +12,12 @@ httpx_probe() {
     httpx -l "$hosts_file" -o "$output_file"
 }
 
+httpx_filter_dupe() {
+    local hosts_file=$1
+    local output_file="$2"
+    httpx -l -fd "$hosts_file" -o "$output_file"
+}
+
 subdomain_takeover_scan() {
     local live_hosts_file=$1
     local output_file=$2
@@ -19,7 +25,7 @@ subdomain_takeover_scan() {
 }
 
 katana_crawl() {
-    local live_hosts_file=$1
+    local filtered_live_hosts_file=$1
     local output_file=$2
     local depth_to_crawl=$3
     local max_duration_to_crawl=$4
@@ -27,8 +33,8 @@ katana_crawl() {
 
     mkdir -p ".cache/$scan_time"
 
-    katana -list "$live_hosts_file" -ps -o ".cache/$scan_time/passive_crawled.txt"
-    katana -list "$live_hosts_file" -d "$depth_to_crawl" -jc -kf -fx -xhr -aff -jsl -c 100 \
+    katana -list "$filtered_live_hosts_file" -ps -o ".cache/$scan_time/passive_crawled.txt"
+    katana -list "$filtered_live_hosts_file" -d "$depth_to_crawl" -jc -kf -fx -xhr -aff -jsl -c 100 \
                 -o ".cache/$scan_time/active_crawled.txt" -ct "$max_duration_to_crawl"
     
     cat ".cache/$scan_time/passive_crawled.txt" ".cache/$scan_time/active_crawled.txt" | sort -u >> "$output_file"
@@ -45,10 +51,11 @@ recon() {
     local output_dir=$2
     mkdir -p $output_dir
     subfinder_scan $domain $output_dir/"subs.txt"
-    httpx_probe $output_dir/"subs.txt" $output_dir/"httpx.txt"
-    subdomain_takeover_scan $output_dir/"httpx.txt" $output_dir/"subdomain_takeover_scan.txt"
-    katana_crawl $output_dir/"httpx.txt" $output_dir/"katana.txt" 3 15m # depth and max duration to crawl
-    nuclei_dast_scan $output_dir/"katana.txt" $output_dir/"nuclei_dastScan_out.txt"
+    httpx_probe $output_dir/"subs.txt" $output_dir/"livehosts.txt"
+    httpx_filter_dupe $output_dir/"livehosts.txt" $output_dir/"filtered_livehosts.txt"
+    subdomain_takeover_scan $output_dir/"livehosts.txt" $output_dir/"subdomain_takeover_scan.txt"
+    katana_crawl $output_dir/"filtered_livehosts.txt" $output_dir/"crawled.txt" 6 15m # depth and max duration to crawl
+    nuclei_dast_scan $output_dir/"crawled.txt" $output_dir/"nuclei_dastscan_out.txt"
 }
 
 if [[ $# -ne 2 ]]; then
